760330ec9deeeff576d333d8f6646941c70c44ab
Gabryela Nagazawa Hayashi <gabynhp@hotmail.com>
2022-06-22 14:51:47 -0300

    bomba

---
+++ trybe-exercicios/fundamentos/bloco4-js/js-primeiros-passos/variaveis.js
@@ -0,0 +1,10 @@
+const myName = "Gabryela";
+const birthCity = "Osvaldo Cruz";
+let birthYear = 1992;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Curitiba";
+console.log(birthCity);
